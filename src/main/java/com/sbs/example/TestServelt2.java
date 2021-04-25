6bf94a492058a9d545fb424007e19b760d4b678e
NTL <58549685+brad0520@users.noreply.github.com>
2021-04-25 16:07:06 +0900

    updates

--- src/main/java/com/sbs/example/TestServelt2.java
+++ src/main/java/com/sbs/example/TestServelt2.java
@@ -15,24 +15,45 @@ public class TestServelt2 extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		System.out.println("서블릿 요청 받음");
 
-		response.setCharacterEncoding("utf-8");
-		response.setContentType("text/html; charset=utf-8");
-
-		PrintWriter out = response.getWriter();	
-
-		String id = request.getParameter("id");
+		// 여기 작성된 내용대로 응답을 하게 됨
+		System.out.println("서블릿 요청 받음"); // 서버 콘솔에 출력.
 		
-		out.println("Hello~, " + id + " this is Servlet!! <br>");
-		out.println("Hello~, " + id + " this is Servlet!!");
+		response.setCharacterEncoding("utf-8"); // utf-8 인코딩 세팅
+		response.setContentType("text/html; charset=utf-8"); // utf-8 html문서로 응답하겠다.
+		
+		// 고객 브라우저에 출력 -> 문서 생성
+		PrintWriter out = response.getWriter();
+		
+		// html 그려야된다.
+		
+		//클라이언트도 최소한의 데이터는 보내줘야된다.
+		
+		// 클라이언트에서 서버로 데이터 주고 받는 법.
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		boolean isLogin = true;
+		
+		out.println("<h1>메인 페이지!!</h1>");
 
-	}
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+		String name = "";
+		if(id.equals("hong") && pw.equals("hong123")) {
+			name = "홍길동";
+		} else if(id.equals("lee") && pw.equals("lee123")) {
+			name = "이순신";
+		} else {
+			isLogin = false;
+		}
+		
+		
+		// 최소한의 html만
+		if(isLogin) {
+			out.println(name + "님!! 반갑습니다.");
+		} else {
+			out.println("잘못된 회원정보입니다.");
+		}
+		
+		
 	}
 
 }
